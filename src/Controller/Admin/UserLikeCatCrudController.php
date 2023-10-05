c63e639b377cb6ee847d56c53b226ff5589debd4
Bonny ARDITO <ardito.b@gmail.com>
2023-10-05 16:39:33 +0200

    ajout du crud controller ULC

---
+++ src/Controller/Admin/UserLikeCatCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+
+use App\Entity\UserLikeCat;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class UserLikeCatCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return UserLikeCat::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->onlyOnIndex(),
+            AssociationField::new('user')->autocomplete(),
+            AssociationField::new('cat')
+                ->autocomplete(),
+            BooleanField::new('is_liked')
+        ];
+    }
+}
